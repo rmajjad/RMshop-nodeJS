9b46bfd3ede57d1b6cac9fc9c5b1d6eba5a2cae5
Rama Majjad <ramamajjad@gmail,com>
2024-05-02 09:33:57 -0700

    categories

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,10 +3,9 @@ import 'dotenv/config'
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-    cloud_name: 'delgi26yg',
-    api_key: '333189753277433',
-    api_secret: 'CLaNRL-ylh4SraDllGqnSaQ8NBw'
+    cloud_name: process.env.cloud_name,
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret
 });
 
 export default cloudinary;
-
