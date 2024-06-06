b894c90c73ed045f0621bf10b391418838d2da20
rmajjad <ramamajjad5@gmail.com>
2024-06-05 17:24:52 -0700

    valedation

--- src/utils/catchError.js
+++ src/utils/catchError.js
@@ -3,7 +3,7 @@ export const asyncHandler = (func)=>{
         try{
             return await func(req,res,next);
         }catch(error){
-            return res.status(500).json({message:"error", error});
+            return res.status(500).json({message:"error", error:error.message});
         }
     }
 }
